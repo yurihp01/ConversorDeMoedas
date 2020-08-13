4768442a95b6e6a25b8428216d25bf183319538f
Yuri Pedroso <yuri.pedroso@venturus.org.br>
2020-08-13 00:14:31 -0300

    trazendo alguns valores da api

--- app/src/main/java/br/edu/ifsp/scl/conversordemoedas/model/CurrencyConverterApi.kt
+++ app/src/main/java/br/edu/ifsp/scl/conversordemoedas/model/CurrencyConverterApi.kt
@@ -1,7 +1,7 @@
 package br.edu.ifsp.scl.conversordemoedas.model
 
 object CurrencyConverterApi {
-    val URL_BASE = "https://currency-converter5.p.rapidapi.com"
+    val URL_BASE = "https://currency-converter5.p.rapidapi.com/"
     val END_POINT = "/currency/list" // Não é bem um endpoint, mas...
     val X_RAPIDAPI_KEY_FIELD = "x-rapidapi-key"
     val X_RAPIDAPI_KEY_VALUE = "da0f5bfc41msh56785302178ec36p1683c3jsnd80cdbaee9df"
--- app/src/main/java/br/edu/ifsp/scl/conversordemoedas/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/conversordemoedas/view/MainActivity.kt
@@ -2,9 +2,12 @@ package br.edu.ifsp.scl.conversordemoedas.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import br.edu.ifsp.scl.conversordemoedas.R
+import br.edu.ifsp.scl.conversordemoedas.model.AvailableCurrencies
 import br.edu.ifsp.scl.conversordemoedas.viewmodel.CurrencyConverterViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.Observer
@@ -19,18 +22,46 @@ class MainActivity : AppCompatActivity() {
         viewModel = CurrencyConverterViewModel(this)
 
         val spinner: Spinner = findViewById(R.id.spinnerOrigem)
+        val spinnerDestino: Spinner = findViewById(R.id.spinnerDestino)
 
-        ArrayAdapter(
-            this,
-            android.R.layout.simple_spinner_item,
-            arrayListOf(viewModel.carregaSpinnerOrigem().value)
-        ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            spinner.adapter = adapter
+        viewModel.carregaSpinnerOrigem().observe(this,
+            androidx.lifecycle.Observer<AvailableCurrencies> {
+                ArrayAdapter(
+                    this,
+                    android.R.layout.simple_spinner_item,
+                    arrayListOf(it.currencies.AED, it.currencies.ALL, it.currencies.AMD, it.currencies.ANG, it.currencies.ARS, it.currencies.AUD, it.currencies.BRL, it.currencies.EUR)
+                ).also { adapter ->
+                    // Specify the layout to use when the list of choices appears
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    // Apply the adapter to the spinner
+                    spinner.adapter = adapter
+                }
+             })
+
+        viewModel.carregaSpinnerDestino().observe(this,
+            androidx.lifecycle.Observer<AvailableCurrencies> {
+                ArrayAdapter(
+                    this,
+                    android.R.layout.simple_spinner_item,
+                    arrayListOf(it.currencies.AED, it.currencies.ALL, it.currencies.AMD, it.currencies.ANG, it.currencies.ARS, it.currencies.AUD, it.currencies.BRL, it.currencies.EUR)
+                ).also { adapter ->
+                    // Specify the layout to use when the list of choices appears
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    // Apply the adapter to the spinner
+                    spinnerDestino.adapter = adapter
+                }
+            })
+
+        spinnerDestino.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                TODO("Not yet implemented")
+            }
+
         }
-
     }
 
     //metodo de lista
